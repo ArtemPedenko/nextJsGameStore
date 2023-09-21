a655e7a8261e863ce83b9b568cf4480de73eae5b
Артем Педенко <131644157+ArtemPedenko@users.noreply.github.com>
2023-09-21 12:44:13 +0000

    Update ModuleBreaker.tsx

--- app/[locale]/components/ModuleBreaker.tsx
+++ app/[locale]/components/ModuleBreaker.tsx
@@ -4,6 +4,15 @@ import styled from "styled-components";
 import { FC, useEffect, useState, useRef } from "react";
 import { useI18n } from "@/locales/client";
 
+const Wrapper = styled.div`
+  width: 1427px;
+  height: 550px;
+  display: flex;
+  flex-direction: row;
+  margin: 0 auto;
+  justify-content: space-between;
+`;
+
 interface ModuleBreakerProps {
   data: any;
 }
@@ -11,7 +20,25 @@ interface ModuleBreakerProps {
 const ModuleBreaker: FC<ModuleBreakerProps> = ({ data }) => {
   const t = useI18n();
   console.log(data.modules);
-  return <></>;
+  return (
+    <Wrapper>
+      {data.modules.map((item, index) => {
+        return (
+          <img
+            alt=""
+            src={item.image.src}
+            style={{
+              height: "350px",
+              width: `${
+                (1427 - data.modules.length * 20) / data.modules.length + "px"
+              }`,
+              objectFit: "cover",
+            }}
+          />
+        );
+      })}
+    </Wrapper>
+  );
 };
 
 export default ModuleBreaker;
