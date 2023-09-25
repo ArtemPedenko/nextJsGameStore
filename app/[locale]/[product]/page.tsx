d3a9acd50601b3d418f8ccb0b6b9f7a92acb8c39
Артем Педенко <131644157+ArtemPedenko@users.noreply.github.com>
2023-09-25 13:25:17 +0000

    Update page.tsx

--- app/[locale]/[product]/page.tsx
+++ app/[locale]/[product]/page.tsx
@@ -1,9 +1,16 @@
+const ProductPage = async ({ params }) => {
+  const url = `https://store-content-ipv4.ak.epicgames.com/api/ru/content/products/${params.product}`;
+  const response = await fetch(url);
 
-export default function ProductPage({ params }) {
-  console.log(params.product)
+  const responseData = await response.json();
+
+  console.log(responseData.pages[0].data.about.description);
   return (
     <div>
       <h1>Product ID: {params.product}</h1>
+      <div>{JSON.stringify(responseData.pages[0].data.about.description)}</div>
     </div>
   );
-} 
+};
+
+export default ProductPage;
