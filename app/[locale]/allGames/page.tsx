ea638a1aa392b9a940bcbbe1442388cccaf6c984
Артем Педенко <131644157+ArtemPedenko@users.noreply.github.com>
2023-10-04 13:02:27 +0000

    Update page.tsx and HeaderSticky.tsx

--- app/[locale]/allGames/page.tsx
+++ app/[locale]/allGames/page.tsx
@@ -1,13 +1,20 @@
-const allGamesPage = async () => {
+const allGamesPage = async ({ params }) => {
   async function getData(url: string) {
     const response = await fetch(url);
     const responseData = await response.json();
     return responseData;
   }
 
+  const url = `https://egs-platform-service.store.epicgames.com/api/v1/egs/category-cards?count=100&country=RU&locale=${params.locale}&start=0&store=EGS`;
+  const genres = await getData(url);
+  console.log(genres.offerDataObj);
   return (
     <>
-      <div>все игры</div>
+      <div>
+        {genres.data.map((item, index) => {
+          return <div key={index}>{item.images[0]}</div>;
+        })}
+      </div>
     </>
   );
 };
