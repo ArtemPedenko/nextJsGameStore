99cba99add6da4e9782d896a8cbe228903ff467e
ArtemPedenko <131644157+ArtemPedenko@users.noreply.github.com>
2023-10-13 08:38:17 +0000

    style module breaker

--- app/[locale]/components/ModuleBreaker.tsx
+++ app/[locale]/components/ModuleBreaker.tsx
@@ -1,59 +1,60 @@
 // @ts-nocheck
-'use client';
+"use client";
 
-import styled from 'styled-components';
-import Link from 'next/link';
-import Image from 'next/image';
-import { FC, useEffect, useState, useRef } from 'react';
-import { useI18n } from '@/locales/client';
-import { v4 as uuid } from 'uuid';
+import styled from "styled-components";
+import Link from "next/link";
+import Image from "next/image";
+import { FC, useEffect, useState, useRef } from "react";
+import { useI18n } from "@/locales/client";
+import { v4 as uuid } from "uuid";
 
 const Wrapper = styled.div`
-	max-width: 1427px;
-	width: 90%;
-	height: 450px;
-	display: flex;
-	flex-direction: row;
-	margin: 15px auto;
-	justify-content: space-between;
-	@media (max-width: 1540px) {
-		flex-direction: column;
-		align-items: center;
-		height: 100%;
-	}
+  max-width: 1427px;
+  width: 90%;
+  height: 450px;
+  display: flex;
+  flex-direction: row;
+  margin: 15px auto;
+  justify-content: space-between;
+  @media (max-width: 1540px) {
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+  }
 `;
 
 interface ModuleBreakerProps {
-	data: any;
+  data: any;
 }
 
 const ModuleBreaker: FC<ModuleBreakerProps> = ({ data }) => {
-	const t = useI18n();
-	return (
-		<Wrapper>
-			{data.modules.map((item, index) => {
-				return (
-					<div
-						key={uuid()}
-						style={{ display: 'flex', flexDirection: 'column', gap: '15px' }}
-					>
-						<img
-							alt=''
-							src={item.image.src}
-							style={{
-								height: '350px',
-								width: `${
-									(1427 - data.modules.length * 20) / data.modules.length + 'px'
-								}`,
-								objectFit: 'cover',
-							}}
-						/>
-						{item.title}
-					</div>
-				);
-			})}
-		</Wrapper>
-	);
+  const t = useI18n();
+  return (
+    <Wrapper>
+      {data.modules.map((item, index) => {
+        return (
+          <div
+            key={uuid()}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "15px",
+            }}
+          >
+            <img
+              alt=""
+              src={item.image.src}
+              style={{
+                width: "100%",
+                objectFit: "cover",
+              }}
+            />
+            {item.title}
+          </div>
+        );
+      })}
+    </Wrapper>
+  );
 };
 
 export default ModuleBreaker;
