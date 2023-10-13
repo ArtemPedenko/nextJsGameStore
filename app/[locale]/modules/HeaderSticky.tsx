e7e484221c5a67179544c91082bebd4396fb906a
ArtemPedenko <nubostrel@gmail.com>
2023-10-13 21:15:14 +0300

    styling header sticky for mobile

--- app/[locale]/modules/HeaderSticky.tsx
+++ app/[locale]/modules/HeaderSticky.tsx
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 import SearchField from './HeaderSticky/SearchField';
 import HeaderStickyButton from './HeaderSticky/HeaderStickyButton';
 import { useI18n } from '@/locales/client';
-
-import { ReactElement } from 'react';
+import ArrowDown from '@/images/ArrowDown';
+import { useState } from 'react';
 
 const Wrapper = styled.div`
 	height: 100px;
@@ -28,16 +28,69 @@ const StickyHead = styled.div`
 	gap: 0 20px;
 `;
 
+const SickyHeaderExpanded = styled.div`
+	@media (max-width: 768px) {
+		display: none;
+	}
+`;
+
+const StickyHeaderMinimized = styled.div`
+	height: 100%;
+	display: none;
+	position: relative;
+	@media (max-width: 768px) {
+		display: block;
+		display: flex;
+		align-items: center;
+	}
+`;
+
+const StickyHeaderMinimizedOpen = styled.div`
+	position: absolute;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 30px;
+	width: 200%;
+	padding: 30px 0;
+	top: 100%;
+	left: -50%;
+	background-color: #121212;
+	z-index: 12;
+`;
+
 export default function HeaderSticky() {
 	const t = useI18n();
-
+	const [menuOpen, setMenuOpen] = useState(false);
+	function olo() {
+		setMenuOpen(!menuOpen);
+	}
 	return (
 		<Wrapper>
 			<StickyHead>
 				<SearchField />
-				<HeaderStickyButton href='/'>{t(`discover`)}</HeaderStickyButton>
-				<HeaderStickyButton href='/allGames'>{t(`browse`)}</HeaderStickyButton>
-				<HeaderStickyButton href='/'>{t(`news`)}</HeaderStickyButton>
+				<SickyHeaderExpanded>
+					<HeaderStickyButton href='/'>{t(`discover`)}</HeaderStickyButton>
+					<HeaderStickyButton href='/allGames'>
+						{t(`browse`)}
+					</HeaderStickyButton>
+					<HeaderStickyButton href='/'>{t(`news`)}</HeaderStickyButton>
+				</SickyHeaderExpanded>
+				<StickyHeaderMinimized>
+					<HeaderStickyButton onClick={olo}>
+						{t(`discover`)}
+						<ArrowDown />
+					</HeaderStickyButton>
+					{menuOpen ? (
+						<StickyHeaderMinimizedOpen>
+							<HeaderStickyButton href='/'>{t(`discover`)}</HeaderStickyButton>
+							<HeaderStickyButton href='/allGames'>
+								{t(`browse`)}
+							</HeaderStickyButton>
+							<HeaderStickyButton href='/'>{t(`news`)}</HeaderStickyButton>
+						</StickyHeaderMinimizedOpen>
+					) : null}
+				</StickyHeaderMinimized>
 			</StickyHead>
 		</Wrapper>
 	);
