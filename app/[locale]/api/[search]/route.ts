2e4739490e8534597cc73de61dc1a6f3132c761b
Артем Педенко <131644157+ArtemPedenko@users.noreply.github.com>
2023-10-17 13:39:55 +0000

    add func to fetch games from wishlist

--- app/[locale]/api/[search]/route.ts
+++ app/[locale]/api/[search]/route.ts
@@ -3,7 +3,7 @@ import { type NextRequest } from "next/server";
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { locale: string; search: string } }
+  { params }: { params: { locale: string; search: string } },
 ) {
   const searchParams = request.nextUrl.searchParams;
   //console.log(searchParams.get("search"));
@@ -17,7 +17,7 @@ export async function GET(
   }
   if (params.search === "searchfield") {
     const search = `https://store.epicgames.com/graphql?operationName=primarySearchAutocomplete&variables=%7B%22allowCountries%22:%22RU%22,%22category%22:%22games%2Fedition%2Fbase%7Cbundles%2Fgames%7Ceditors%7Csoftware%2Fedition%2Fbase%7Caddons%2Flaunchable%22,%22count%22:4,%22country%22:%22RU%22,%22keywords%22:%22${searchParams.get(
-      "search"
+      "search",
     )}%22,%22locale%22:%22${
       params.locale
     }%22,%22sortBy%22:null,%22sortDir%22:%22DESC%22%7D&extensions=%7B%22persistedQuery%22:%7B%22version%22:1,%22sha256Hash%22:%22531ca97218358754b2a3dade40dbbfc62e280d0173dcaf53305b3b3f3c393580%22%7D%7D`;
@@ -27,6 +27,17 @@ export async function GET(
   //
   // Function to search games for wishlist page
   if (params.search === "wishlist") {
+    const offerUrl = `https://store.epicgames.com/graphql?operationName=getCatalogOffer&variables=%7B%22locale%22:%22${
+      params.locale
+    }%22,%22country%22:%22RU%22,%22offerId%22:%22${searchParams.get(
+      "id",
+    )}%22,%22sandboxId%22:%22${searchParams.get(
+      "namespace",
+    )}%22%7D&extensions=%7B%22persistedQuery%22:%7B%22version%22:1,%22sha256Hash%22:%22c4ad546ad2757b60ff13ace19ffaf134abb23cb663244de34771a0444abfdf33%22%7D%7D`;
+    //console.log(searchParams.get("id"));
+    //console.log(searchParams.get("namespace"));
+    const wishlistData = await getData(offerUrl);
+    return new NextResponse(JSON.stringify(wishlistData));
   }
   //
 }
--- app/[locale]/wishlist/page.tsx
+++ app/[locale]/wishlist/page.tsx
@@ -9,6 +9,29 @@ import { useEffect, useState } from "react";
 const WishlistPage = () => {
   const userData = useAppSelector((state) => state.games.userData);
   const [wishlist, setWishlist] = useState({});
+  const [wishlistData, setWishListData] = useState("");
+  let gamesArray: any[] = [];
+  async function getWishlistData(id: string, namespace: string) {
+    fetch(`/en/api/wishlist?id=${id}&namespace=${namespace}`)
+      .then(function (serverPromise) {
+        serverPromise
+          .json()
+          .then(function (data) {
+            gamesArray.push(data);
+          })
+          .catch(function (e) {
+            console.log(e);
+          });
+      })
+      .catch(function (e) {
+        console.log(e);
+      });
+  }
+
+  Object.keys(wishlist).map((item: string) => {
+    getWishlistData(wishlist[item].id, wishlist[item].namespace);
+    console.log(gamesArray);
+  });
 
   useEffect(() => {
     getUserWishlist(userData.email).then((response) => {
