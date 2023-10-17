51e8645d68cd46ac3528aec93d6c647e5acf6b43
ArtemPedenko <131644157+ArtemPedenko@users.noreply.github.com>
2023-10-17 06:37:04 +0000

    understanding Cloud Firestore

--- app/[locale]/wishlist/page.tsx
+++ app/[locale]/wishlist/page.tsx
@@ -1,67 +1,58 @@
-'use client';
+"use client";
 
-import { db } from '@/app/firebase/firebase';
-import { useAppDispatch, useAppSelector } from '@/app/store/hooks';
+import { db } from "@/app/firebase/firebase";
+import { useAppDispatch, useAppSelector } from "@/app/store/hooks";
 import {
-	collection,
-	doc,
-	setDoc,
-	getDoc,
-	getDocs,
-	query,
-} from 'firebase/firestore';
+  collection,
+  doc,
+  setDoc,
+  getDoc,
+  getDocs,
+  deleteDoc,
+  query,
+  updateDoc,
+  deleteField,
+  FieldValue,
+} from "firebase/firestore";
 
 const WishlistPage = () => {
-	const userData = useAppSelector((state) => state.games.userData);
+  const userData = useAppSelector((state) => state.games.userData);
 
-	async function setUserData() {
-		const citiesRef = collection(db, 'usersData');
-		const wishList = {
-			['game1']: {
-				id: 'ololo',
-				namespace: '2',
-			},
-			game2: {
-				id: '1',
-				namespace: '2',
-			},
-		};
+  async function addData() {
+    const docRef = doc(db, "usersData", userData.email + "&wishList");
+    setDoc(
+      docRef,
+      { game4: { id: "12321", namespace: "ffkf;l" } },
+      { merge: true }
+    );
+  }
 
-		const cart = {
-			game3: {
-				id: '1',
-				namespace: '2',
-			},
-			game4: {
-				id: '1',
-				namespace: '2',
-			},
-		};
+  async function getUserData() {
+    const docRef = doc(db, "usersData", userData.email + "&wishList");
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      //console.log(docSnap.data().cart);
+      console.log(docSnap.data());
+    } else {
+      // docSnap.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  }
 
-		await setDoc(doc(citiesRef, userData.email), {
-			wishList: wishList,
-			cart: cart,
-		});
-	}
+  async function deleteGame() {
+    const cityRef = doc(db, "usersData", userData.email + "&wishList");
+    await updateDoc(cityRef, {
+      game2: deleteField(),
+    });
+  }
 
-	async function getUserData() {
-		const docRef = doc(db, 'usersData', userData.email);
-		const docSnap = await getDoc(docRef);
-		if (docSnap.exists()) {
-			console.log(docSnap.data().cart);
-			console.log(docSnap.data());
-		} else {
-			// docSnap.data() will be undefined in this case
-			console.log('No such document!');
-		}
-	}
-
-	return (
-		<div>
-			<button onClick={() => setUserData()}>add user data</button>
-			<button onClick={() => getUserData()}>get user data</button>
-		</div>
-	);
+  return (
+    <div>
+      <button onClick={() => addData()}>addData</button>
+      <button onClick={() => getUserData()}>get user data</button>
+      <button onClick={() => deleteGame()}>delete game</button>
+    </div>
+  );
 };
 
 export default WishlistPage;
