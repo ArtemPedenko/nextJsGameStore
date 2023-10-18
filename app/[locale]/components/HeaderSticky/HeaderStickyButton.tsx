676c1c00bd3c59704f002cfd631d7693dc1cc631
ArtemPedenko <131644157+ArtemPedenko@users.noreply.github.com>
2023-10-18 09:28:43 +0000

    cant delete any type

--- app/[locale]/components/HeaderSticky/HeaderStickyButton.tsx
+++ app/[locale]/components/HeaderSticky/HeaderStickyButton.tsx
@@ -1,48 +1,48 @@
-import styled from 'styled-components';
-import { FC } from 'react';
-import Link from 'next/link';
+import styled from "styled-components";
+import { FC } from "react";
+import Link from "next/link";
 
 const Button = styled.button`
-	height: 100%;
-	border: none;
-	background-color: #121212;
-	font-size: 14px;
-	color: #b8b8b8;
-	padding: 0 10px;
-	outline: none;
-	text-decoration: none;
-	display: inline-flex;
-	align-items: center;
-	position: relative;
-	overflow: hidden;
-	font-family: OpenSans, sans-serif, arial;
-	letter-spacing: 0.075em;
-	&:hover {
-		cursor: pointer;
-		color: #ffffff;
-	}
+  height: 100%;
+  border: none;
+  background-color: #121212;
+  font-size: 14px;
+  color: #b8b8b8;
+  padding: 0 10px;
+  outline: none;
+  text-decoration: none;
+  display: inline-flex;
+  align-items: center;
+  position: relative;
+  overflow: hidden;
+  font-family: OpenSans, sans-serif, arial;
+  letter-spacing: 0.075em;
+  &:hover {
+    cursor: pointer;
+    color: #ffffff;
+  }
 `;
 
 interface HeaderButtonProps {
-	children: any;
-	href?: string;
-	onClick?: React.InputHTMLAttributes<HTMLButtonElement>['onClick'];
+  children: any;
+  href?: string;
+  onClick?: React.InputHTMLAttributes<HTMLButtonElement>["onClick"];
 }
 
 const HeaderStickyButton: FC<HeaderButtonProps> = ({
-	children,
-	href,
-	onClick,
+  children,
+  href,
+  onClick,
 }) => {
-	if (href) {
-		return (
-			<Link href={href} style={{ height: '100%' }}>
-				<Button onClick={onClick}>{children}</Button>
-			</Link>
-		);
-	} else {
-		return <Button onClick={onClick}>{children}</Button>;
-	}
+  if (href) {
+    return (
+      <Link href={href} style={{ height: "100%" }}>
+        <Button onClick={onClick}>{children}</Button>
+      </Link>
+    );
+  } else {
+    return <Button onClick={onClick}>{children}</Button>;
+  }
 };
 
 export default HeaderStickyButton;
