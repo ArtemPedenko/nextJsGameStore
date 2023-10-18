6686b55e40758bb8b9b26c012f8c9e2fd08ed608
ArtemPedenko <131644157+ArtemPedenko@users.noreply.github.com>
2023-10-18 09:04:22 +0000

    correct types and delete useless imports/variables

--- app/[locale]/authorization/components/Login.tsx
+++ app/[locale]/authorization/components/Login.tsx
@@ -1,135 +1,123 @@
-'use client';
+"use client";
 
-import { FC, MouseEventHandler } from 'react';
-import SiteLogo from '@/images/SiteLogo';
-import IconWrapper from '../../components/IconWrapper';
-import styled from 'styled-components';
-import { useI18n } from '@/locales/client';
-import Button from '../../components/Button';
-import { auth } from '../../../firebase/firebase';
-import {
-	createUserWithEmailAndPassword,
-	signInWithEmailAndPassword,
-	onAuthStateChanged,
-	signOut,
-	updateProfile,
-} from 'firebase/auth';
-import { useState, useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '@/app/store/hooks';
-import { setUserData } from '@/app/store/slice';
-import { useRouter } from 'next/navigation';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
+import { FC } from "react";
+import SiteLogo from "@/images/SiteLogo";
+import IconWrapper from "../../components/IconWrapper";
+import styled from "styled-components";
+import { useI18n } from "@/locales/client";
+import Button from "../../components/Button";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 interface LoginProps {
-	loginUser: Function;
-	setPageView: Function;
-	userError: boolean;
-	setUserError: Function;
-	setRegisterError: Function;
+  loginUser: Function;
+  setPageView: Function;
+  userError: boolean;
+  setUserError: Function;
+  setRegisterError: Function;
 }
 
 const Input = styled.input`
-	width: 100%;
-	height: 50px;
-	background-color: #2a2a2a;
-	border: 1px solid #686868;
-	border-radius: 3px;
-	color: white;
-	padding: 0;
-	&:focus {
-		border: 1px solid white;
-	}
+  width: 100%;
+  height: 50px;
+  background-color: #2a2a2a;
+  border: 1px solid #686868;
+  border-radius: 3px;
+  color: white;
+  padding: 0;
+  &:focus {
+    border: 1px solid white;
+  }
 `;
 
 const Form = styled.form`
-	display: flex;
-	flex-direction: column;
-	gap: 10px;
-	width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
 `;
 
 const Login: FC<LoginProps> = ({
-	loginUser,
-	setPageView,
-	userError,
-	setUserError,
-	setRegisterError,
+  loginUser,
+  setPageView,
+  userError,
+  setUserError,
+  setRegisterError,
 }) => {
-	const t = useI18n();
+  const t = useI18n();
 
-	return (
-		<>
-			<IconWrapper
-				icon={<SiteLogo />}
-				height='100px'
-				width='100px'
-				margin='0 10px'
-			/>
-			<Formik
-				initialValues={{ email: '', password: '' }}
-				onSubmit={(values) => loginUser(values.email, values.password)}
-				validateOnChange={false}
-				validateOnBlur={false}
-				validationSchema={Yup.object().shape({
-					email: Yup.string().email().required('Обязательное поле'),
-					password: Yup.string().min(6).required('Обязательное поле'),
-				})}
-			>
-				{(props) => {
-					props.submitCount > 0 && (props.validateOnChange = true);
-					const { values, errors, handleChange, handleSubmit } = props;
-					return (
-						<Form onSubmit={handleSubmit}>
-							<Input
-								id='email'
-								placeholder={t(`enter_email`)}
-								type='text'
-								value={values.email}
-								onChange={handleChange}
-							/>
-							{errors.email ? (
-								<div style={{ color: 'red' }}>{errors.email}</div>
-							) : null}
+  return (
+    <>
+      <IconWrapper
+        icon={<SiteLogo />}
+        height="100px"
+        width="100px"
+        margin="0 10px"
+      />
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => loginUser(values.email, values.password)}
+        validateOnChange={false}
+        validateOnBlur={false}
+        validationSchema={Yup.object().shape({
+          email: Yup.string().email().required("Обязательное поле"),
+          password: Yup.string().min(6).required("Обязательное поле"),
+        })}
+      >
+        {(props) => {
+          props.submitCount > 0 && (props.validateOnChange = true);
+          const { values, errors, handleChange, handleSubmit } = props;
+          return (
+            <Form onSubmit={handleSubmit}>
+              <Input
+                id="email"
+                placeholder={t(`enter_email`)}
+                type="text"
+                value={values.email}
+                onChange={handleChange}
+              />
+              {errors.email ? (
+                <div style={{ color: "red" }}>{errors.email}</div>
+              ) : null}
 
-							<Input
-								id='password'
-								placeholder={t(`enter_password`)}
-								type='password'
-								value={values.password}
-								onChange={handleChange}
-							/>
-							{errors.password ? (
-								<div style={{ color: 'red' }}>{errors.password}</div>
-							) : null}
+              <Input
+                id="password"
+                placeholder={t(`enter_password`)}
+                type="password"
+                value={values.password}
+                onChange={handleChange}
+              />
+              {errors.password ? (
+                <div style={{ color: "red" }}>{errors.password}</div>
+              ) : null}
 
-							<Button type='submit' onClick={() => null}>
-								{t(`sign_in`)}
-							</Button>
-						</Form>
-					);
-				}}
-			</Formik>
-			{userError ? (
-				<div style={{ color: 'red' }}>{t(`wrong_email_password`)}</div>
-			) : null}
-			{
-				<>
-					<div>{t(`dont_have_account`)}</div>
-					<div
-						onClick={() => {
-							setPageView('registration');
-							setUserError(false);
-							setRegisterError('');
-						}}
-						style={{ textDecoration: 'underline', cursor: 'pointer' }}
-					>
-						{t(`sign_up`)}
-					</div>
-				</>
-			}
-		</>
-	);
+              <Button type="submit" onClick={() => null}>
+                {t(`sign_in`)}
+              </Button>
+            </Form>
+          );
+        }}
+      </Formik>
+      {userError ? (
+        <div style={{ color: "red" }}>{t(`wrong_email_password`)}</div>
+      ) : null}
+      {
+        <>
+          <div>{t(`dont_have_account`)}</div>
+          <div
+            onClick={() => {
+              setPageView("registration");
+              setUserError(false);
+              setRegisterError("");
+            }}
+            style={{ textDecoration: "underline", cursor: "pointer" }}
+          >
+            {t(`sign_up`)}
+          </div>
+        </>
+      }
+    </>
+  );
 };
 
 export default Login;
--- app/[locale]/authorization/page.tsx
+++ app/[locale]/authorization/page.tsx
@@ -1,162 +1,138 @@
-// @ts-nocheck
-'use client';
+"use client";
 
-import styled from 'styled-components';
-import { useI18n } from '@/locales/client';
-import { auth } from '@/app/firebase/firebase';
+import styled from "styled-components";
+import { auth } from "@/app/firebase/firebase";
 import {
-	createUserWithEmailAndPassword,
-	signInWithEmailAndPassword,
-	signOut,
-	updateProfile,
-} from 'firebase/auth';
-import { useState } from 'react';
-import { useAppDispatch, useAppSelector } from '@/app/store/hooks';
-import { setUserData } from '@/app/store/slice';
-import { useRouter } from 'next/navigation';
-import Login from './components/Login';
-import Registration from './components/Registration';
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "@/app/store/hooks";
+import { setUserData } from "@/app/store/slice";
+import { useRouter } from "next/navigation";
+import Login from "./components/Login";
+import Registration from "./components/Registration";
 
 const AuthorizationPage = styled.div`
-	position: absolute;
-	top: 50px;
-	width: 100vw;
-	height: 100vh;
-	background-color: black;
-	z-index: 12;
+  position: absolute;
+  top: 50px;
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+  z-index: 12;
 `;
 const AuthorizationContentWrapper = styled.div`
-	max-width: 360px;
-	width: 90%;
-	height: 500px;
-	padding: 50px;
-	background-color: #2a2a2a;
-	margin: 25px auto;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	gap: 10px;
+  max-width: 360px;
+  width: 90%;
+  height: 500px;
+  padding: 50px;
+  background-color: #2a2a2a;
+  margin: 25px auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
 `;
 
-const Input = styled.input`
-	width: 100%;
-	height: 50px;
-	background-color: #2a2a2a;
-	border: 1px solid #686868;
-	border-radius: 3px;
-	color: white;
-	padding: 0;
-	&:focus {
-		border: 1px solid white;
-	}
-`;
+const Authorization = (params: { locale: string }) => {
+  const dispatch = useAppDispatch();
+  const [pageView, setPageView] = useState("login");
+  const [userError, setUserError] = useState(false);
+  const [registrError, setRegisterError] = useState("");
 
-const Form = styled.form`
-	display: flex;
-	flex-direction: column;
-	gap: 10px;
-	width: 100%;
-`;
+  const { push } = useRouter();
 
-const Authorization = ({ searchParams, params }) => {
-	const t = useI18n();
-	const dispatch = useAppDispatch();
-	const userData = useAppSelector((state) => state.games.userData);
-	const [pageView, setPageView] = useState('login');
-	const [userError, setUserError] = useState(false);
-	const [registrError, setRegisterError] = useState('');
+  async function registerUser(
+    userEmail: string,
+    userPass: string,
+    userName: string
+  ) {
+    /* console.log(userEmail, userPass); */
+    await createUserWithEmailAndPassword(auth, userEmail, userPass)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        dispatch(setUserData(user));
+        // ...
+      })
+      .then(() => {
+        if (auth.currentUser) {
+          updateProfile(auth.currentUser, {
+            displayName: userName,
+          });
+        }
+        push("./");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        if (errorCode == "auth/email-already-in-use") {
+          if (params.locale === "ru") {
+            setRegisterError("Этот email уже используется");
+          } else {
+            setRegisterError("This email already in use");
+          }
+        }
+        // ..
+      });
+  }
 
-	const { push } = useRouter();
+  async function loginUser(userEmail: string, userPass: string) {
+    /* console.log(userEmail, userPass); */
 
-	async function registerUser(
-		userEmail: string,
-		userPass: string,
-		userName: string
-	) {
-		/* console.log(userEmail, userPass); */
-		await createUserWithEmailAndPassword(auth, userEmail, userPass)
-			.then((userCredential) => {
-				// Signed in
-				const user = userCredential.user;
-				dispatch(setUserData(user));
-				// ...
-			})
-			.then(() => {
-				if (auth.currentUser) {
-					updateProfile(auth.currentUser, {
-						displayName: userName,
-					});
-				}
-				push('./');
-			})
-			.catch((error) => {
-				const errorCode = error.code;
-				const errorMessage = error.message;
-				console.log(errorCode);
-				if (errorCode == 'auth/email-already-in-use') {
-					if (params.locale === 'ru') {
-						setRegisterError('Этот email уже используется');
-					} else {
-						setRegisterError('This email already in use');
-					}
-				}
-				// ..
-			});
-	}
+    signInWithEmailAndPassword(auth, userEmail, userPass)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        dispatch(setUserData(user));
+        push("./");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        setUserError(true);
+      });
+  }
 
-	async function loginUser(userEmail: string, userPass: string) {
-		/* console.log(userEmail, userPass); */
+  async function sighOut() {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        //console.log("Signed out successfully");
+      })
+      .catch((error) => {
+        console.log(error); // An error happened.
+      });
+  }
 
-		signInWithEmailAndPassword(auth, userEmail, userPass)
-			.then((userCredential) => {
-				// Signed in
-				const user = userCredential.user;
-				dispatch(setUserData(user));
-				push('./');
-			})
-			.catch((error) => {
-				const errorCode = error.code;
-				const errorMessage = error.message;
-				console.log(errorCode, errorMessage);
-				setUserError(true);
-			});
-	}
-
-	async function sighOut() {
-		signOut(auth)
-			.then(() => {
-				// Sign-out successful.
-				//console.log("Signed out successfully");
-			})
-			.catch((error) => {
-				console.log(error); // An error happened.
-			});
-	}
-
-	return (
-		<AuthorizationPage>
-			<AuthorizationContentWrapper>
-				{pageView === 'login' ? (
-					<Login
-						loginUser={loginUser}
-						userError={userError}
-						setUserError={setUserError}
-						setPageView={setPageView}
-						setRegisterError={setRegisterError}
-					/>
-				) : (
-					<Registration
-						registerUser={registerUser}
-						userError={userError}
-						setPageView={setPageView}
-						setUserError={setUserError}
-						registrError={registrError}
-					/>
-				)}
-			</AuthorizationContentWrapper>
-		</AuthorizationPage>
-	);
+  return (
+    <AuthorizationPage>
+      <AuthorizationContentWrapper>
+        {pageView === "login" ? (
+          <Login
+            loginUser={loginUser}
+            userError={userError}
+            setUserError={setUserError}
+            setPageView={setPageView}
+            setRegisterError={setRegisterError}
+          />
+        ) : (
+          <Registration
+            registerUser={registerUser}
+            userError={userError}
+            setPageView={setPageView}
+            setUserError={setUserError}
+            registrError={registrError}
+          />
+        )}
+      </AuthorizationContentWrapper>
+    </AuthorizationPage>
+  );
 };
 
 export default Authorization;
