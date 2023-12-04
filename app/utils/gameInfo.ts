e75504cb649f993b0c48533cfd24c6c89b71d182
ArtemPedenko <nubostrel@gmail.com>
2023-12-04 11:22:32 +0300

    test

--- app/utils/gameInfo.ts
+++ app/utils/gameInfo.ts
@@ -1,13 +1,12 @@
 async function getData(locale: string) {
-	const url = `https://store-site-backend-static-ipv4.ak.epicgames.com/storefrontLayout?locale=${locale}&country=RU&start=0&count=6`
+	const url = `https://store-site-backend-static-ipv4.ak.epicgames.com/storefrontLayout?locale=${locale}&country=RU&start=0&count=6`;
 	const response = await fetch(url)
 		.then((response) => response.json())
 		.then((data) => {
-			
 			return data.data.Storefront.storefrontModulesPaginated.modules;
 		})
 		.catch((error) => console.error(error));
 	return response;
 }
-
+//
 export default getData;
